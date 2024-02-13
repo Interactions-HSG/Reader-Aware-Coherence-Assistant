545cc5bd68e4cb8c219ce35403555b0c3e2f24e5
JC042 <49511520+lg565245594@users.noreply.github.com>
2024-02-13 17:01:54 +0100

    Update app_server.py
    Task: Test two functions
    1. Generate a x length-word paragraph based on input concepts 
    2. Take an input paragraph and get nb_of_keywords

--- app_server.py
+++ app_server.py
@@ -1,6 +1,3 @@
-#server script
-#configure the server-side code to handle the /api/coherence-analysis endpoint
-#this file handles the http requests and responses for your web app
 from arch import *
 
 import openai
@@ -14,11 +11,6 @@ if __name__ == '__main__':
     CORS(app)
 
 
-# Configure your OpenAI API key
-api_key = "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"  # Replace with your actual API key
-openai.api_key = api_key
-
-
 # Server side
 # Define a fetch route and use my_fetch function(defined in the reader_profile.py),to fetch data using personal_id
 @app.route('/fetchGS', methods=['POST'])
@@ -26,51 +18,6 @@ def route_my_fetch():
     personal_id = request.json.get('personal_id')
     return my_fetch(personal_id)
 
-
-'''@app.route('/api/coherence-analysis', methods=['POST'])
-def coherence_analysis():
-    user_input = request.json.get('txt')
-    #combine the profiles,write the prompt(s)
-    selected_profile = request.json.get('profile')
-
-    try:
-        # Construct the prompt based on the selected profile and user input
-        if selected_profile == 'Layman':
-            prompt = (f"Identify complex terms in this technical text and suggest simpler alternatives for a general audience:\n\n"
-          f"{user_input}")
-
-        elif selected_profile == 'Simon':
-            prompt = f"You are an assistant for analyzing coherence between user's text:\n\n{user_input}\n\n and {selected_profile}. Find a section that matches the two."
-        else:
-            # Handle other profiles here if needed
-            pass    
-
-
-    #try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are an assistant for analyzing coherence."},
-                {"role": "user", "content": prompt}
-            ],
-
-        )
-
-        # Extract the combined output and suggestions from the response
-        combined_output = response["choices"][0]["message"]["content"]
-        suggestions = "These are the suggestions."  # Replace with your own logic
-
-        # Return the results as JSON
-        return jsonify({
-            "combinedOutput": combined_output,
-            "suggestions": suggestions
-        })
-    except Exception as e:
-        print(f"Error calling OpenAI API: {str(e)}")
-        return jsonify({
-            "combinedOutput": "An error occurred while processing the request.",
-            "suggestions": "No suggestions received."
-        })'''
 
 # Define the route for POST request, this route is to handle the analysis of user input and abstract
 @app.route('/api/coherence-analysis2', methods=['POST'])
@@ -92,9 +39,9 @@ def coherence_analysis2():
 
         )
 
-        # Extract the combined output and suggestions from the response
+        # Extract the combined_output and suggestions from the response
         combined_output = response["choices"][0]["message"]["content"]
-        suggestions = "These are the suggestions."  # Replace with your own logic
+        suggestions = "These are the suggestions." 
 
         # Return the results as JSON
         return jsonify({
@@ -108,14 +55,100 @@ def coherence_analysis2():
             "suggestions": "No suggestions received."
         })
 
-# Serve merged abstracts when Simon profile is selected
-'''@app.route('/merged-abstracts/simon', methods=['GET'])
-def serve_simon_merged_abstracts():
-    # Define the path to the Simon's merged abstracts file
-    simon_merged_abstracts_path = os.path.join(merged_abstracts_dir, 'simon_abstracts.txt')
-    
-    return send_from_directory(merged_abstracts_dir, 'simon_abstracts.txt')
-'''
+
+@app.route('/get_score', methods=['POST'])
+def get_score():
+    user_input = request.json.get('txt')
+    abstract = request.json.get('abstract')
+
+    dictScores = metrics.score_clauses(user_input, params={
+        'openai_api_key': openai.api_key,
+        'ref_text': abstract
+    })
+
+    print(dictScores)
+
+    return jsonify(dictScores)
+
+@app.route('/demo-srv', methods = ['POST']) #to test one function or another: 1)check which demo we want to ask 2)check the parameter according to the functions 3) prepare the prompts 4)send the prompt 5) return the response
+def my_demo_srv(): #new form with text area to be selected_demo
+    #check which demo we selected
+    selected_demo = request.json.get('selected_demo')
+    param1 = request.json.get('param1')
+    param2 = request.json.get('param2')
+
+
+    response = ''
+    if selected_demo == 'generate paragraph':
+        #generateParagraph
+
+        # 3. Build the prompt and send it
+        try:
+            # build the prompt
+            prompt = f"give me a paragraph in {param2} words, which relates to {param1}"
+            # send the prompt to ChatGPT
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are an assistant for analyzing coherence."},
+                    {"role": "user", "content": prompt}
+                ],
+
+            )
+
+            # Extract the combined output and suggestions from the response
+            gptOutput = response["choices"][0]["message"]["content"]
+
+            # Return the results as JSON
+            return jsonify({
+                "gptOutput": gptOutput
+            })
+        except Exception as e:
+            print(f"Error calling OpenAI API: {str(e)}")
+            return jsonify({
+                "gptOutput": "An error occurred while processing the request.",
+                "errOutput": "Error calling OpenAI API"
+            })
+    elif selected_demo == 'extract keywords':
+        #extractKeywords
+
+        # 3. Build the prompt and send it
+        try:
+            # build the prompt
+            prompt = f"Consider this paragraph: \"{param1}\" \nAbstract {param2} concepts that relate to the main subject of this paragraph. Return only a list of keywords separated by commas."
+            print(prompt)
+            # send the prompt to ChatGPT
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are an assistant for analyzing coherence."},
+                    {"role": "user", "content": prompt}
+                ],
+
+            )
+
+            # Extract the combined output and suggestions from the response
+            gptOutput = response["choices"][0]["message"]["content"]
+
+            # Return the results as JSON
+            return jsonify({
+                "gptOutput": gptOutput
+            })
+        except Exception as e:
+            print(f"Error calling OpenAI API: {str(e)}")
+            return jsonify({
+                "gptOutput": "An error occurred while processing the request.",
+                "errOutput": "Error calling OpenAI API"
+            })
+
+
+    else:
+        #raise error
+        response = '3'
+
+    return jsonify(response)
+
+
 
 def import_main_execution():
     print('app_server imported')
